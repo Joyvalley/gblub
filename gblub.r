c5a0fadea3194b6cd4e7bd0a0b89d8560970033b
Joyvalley <j.a.freudenthal@gmail.com>
2017-09-21 10:06:35 +0200

    full functional gblub function

--- gblub.r
+++ gblub.r
@@ -1,28 +1,61 @@
-### glub 
-## borrowed functions from synbreed ... install.packages("synbreed") doesnt work so Download the functions from https://cran.r-project.org/web/packages/synbreed/index.html
-### you just have to change the directory unless you put in ~/R/synbree
-
-## uncomment the next two lines for first time use to load required packages
-depends <- c("abind","BGLR","doBy","doParallel","foreach","MASS","qtl","regress")
-sapply(depends,function(X){install.packages(X)})
-
-sapply(depends, library, character.only = TRUE)
+### gblup function that gblups
+## works fine if you returns list mod1 for blup and mod3 for bayesian lasso
 
 
-setwd("~/R/synbreed/R")
-syn_funcs <- dir()
-sapply(syn_funcs, function(X) {source(X)})
 
-names(maize)
+pheno <- read.csv("gent_pheno.csv")
+head(pheno)
 
-maizeC <- codeGeno(maize)
-K <- kin(maizeC,ret="kin",DH=maize$covar$DH)
-U <- kin(maizeC,ret="realized")/2
+gblup("gent_pheno.csv","gent_geno.csv")
 
-mod1 <- gpMod(maizeC,model="BLUP",kin=K)
-mod2 <- gpMod(maizeC,model="BLUP",kin=U)
-prior <- list(varE=list(df=3,S=35),lambda = list(shape=0.52,rate=1e-4,value=20,type='random'))
-mod3 <- gpMod(maizeC,model="BL",prior=prior,nIter=6000,burnIn=1000,thin=5)
+y <- mod1$y
+g <- mod1$g
 
-names(mod1)
-mod2[[3]]
+pred <- y + g
+plot(pred,y)
+cor(pred)
+
+
+
+gblup <- function(phenocsv, genocsv, inst = FALSE, req = TRUE, BLUP = TRUE, BL = FALSE){
+
+    depends<- c('synbreed',"BGLR","doBy","doParallel","foreach","MASS","qtl","regress",'R.utils')
+    if(inst == TRUE)    
+        sapply(depends,function(X){install.packages(X)})
+    
+    if(req == TRUE)
+        foo <- sapply(depends,library, character.only = TRUE)
+
+    rm(depends)
+    if(req == TRUE)
+        rm(foo)
+    
+
+
+    geno.csv <- read.csv(genocsv)
+    pheno.csv <- read.csv(phenocsv)
+    my_pheno <- as.data.frame(pheno.csv[,2],row.names = rownames(pheno.csv))
+    colnames(my_pheno) <- "Yield"
+    my_geno <- as.data.frame(geno.csv[,2:dim(geno.csv)[2]], col.names = colnames(geno.csv),row.names=rownames(geno.csv))
+    my_geno <- as.matrix(my_geno)
+      
+
+    my_gp <- create.gpData(pheno=my_pheno,geno=my_geno)
+    myC <- codeGeno(my_gp)
+    K <- kin(myC, ret="realized")/2
+
+    if(BLUP==TRUE)
+        mod1 <<- gpMod(myC,model="BLUP",kin=K)
+
+    if(BL==TRUE){
+        prior <- list(varE=list(df=3,S=35),lambda = list(shape=0.52,rate=1e-4,value=20,type='random'))
+        mod3 <<- gpMod(myC,model="BL",prior=prior,nIter=6000,burnIn=1000,thin=5)}
+         
+}
+
+
+
+
+
+
+
