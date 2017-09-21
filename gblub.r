69a3fdecbc7459904af138df421148c2c5e86545
Joyvalley <j.a.freudenthal@gmail.com>
2017-09-21 11:17:34 +0200

    returns pred acc.

--- gblub.r
+++ gblub.r
@@ -8,12 +8,6 @@ head(pheno)
 
 gblup("gent_pheno.csv","gent_geno.csv")
 
-y <- mod1$y
-g <- mod1$g
-
-pred <- y + g
-plot(pred,y)
-cor(pred)
 
 
 
@@ -44,13 +38,28 @@ gblup <- function(phenocsv, genocsv, inst = FALSE, req = TRUE, BLUP = TRUE, BL =
     myC <- codeGeno(my_gp)
     K <- kin(myC, ret="realized")/2
 
-    if(BLUP==TRUE)
+    if(BLUP==TRUE){
         mod1 <<- gpMod(myC,model="BLUP",kin=K)
+        y <- mod1$y
+        g <- mod1$g
+        pred <- y + g
+        mod1[[7]] <- pred
+        mod1[[8]] <- cor(pred,y)
+        names(mod1)[7:8] <- c("Predicted Phenotypes","Prediction Accuracy")
 
+        
+        if(plot==TRUE)
+             plot(pred,y)
+    }
+    
     if(BL==TRUE){
         prior <- list(varE=list(df=3,S=35),lambda = list(shape=0.52,rate=1e-4,value=20,type='random'))
         mod3 <<- gpMod(myC,model="BL",prior=prior,nIter=6000,burnIn=1000,thin=5)}
          
+
+
+
+
 }
 
 
