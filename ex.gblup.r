09ad3dd567bf83354bb4113e0cfada0d8630c02c
Joyvalley <j.a.freudenthal@gmail.com>
2017-10-06 16:54:33 +0200

    fix

--- ex.gblup.r
+++ ex.gblup.r
@@ -1,13 +1,10 @@
 ## execute this script with: Rscript ex.gblup.r 
 
-
 source("gblub.r")
 args <- commandArgs(trailingOnly = TRUE)
 
 pheno.name <- args[1]
 geno.name <- args[2]
-
-pheno <- read.csv("gent_pheno.csv")
 
 gblup(pheno.name,geno.name)
 
