edb8fb3b4c14da957fb9d7be35472a061a2e03ac
Markus Ankenbrand <markus.ankenbrand@uni-wuerzburg.de>
2017-10-19 08:43:23 +0200

    Implement cross validation with gblup

--- ex.gblup.r
+++ ex.gblup.r
@@ -5,8 +5,13 @@ args <- commandArgs(trailingOnly = TRUE)
 
 pheno.name <- args[1]
 geno.name <- args[2]
+cvf.name <- args[3]
+cvf = 5
+if(length(args) > 3){
+    cvf = as.numeric(args[4])
+}
 
-gblup(pheno.name,geno.name)
+gblup(pheno.name,geno.name,cvf.name,cvf)
 
 
 
