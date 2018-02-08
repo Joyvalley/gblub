b2317fe62ca6903f0eb071710f563f1c584efebd
Joyvalley <j.a.freudenthal@gmail.com>
2018-02-08 12:46:03 +0100

    output file writen

--- ex.gblup.r
+++ ex.gblup.r
@@ -1,15 +1,14 @@
 
 ## execute this script with: Rscript ex.gblup.r -x genofile -y phenofile -c cv file
-source("gblub.r")
+source("/storage/evolgen/jan/gblub/gblub.r")
 
 my.args <- commandArgs(trailingOnly = TRUE)
 #my.args <- c("-x", "gent_geno.csv","-y" , "gent_pheno.csv")
 ### set defaults 
 cvf.name = NA
-cvf = 2
 
 ## parsing the command line options 
-all.opts <- c("-x","-y","-c","-JobId","-label","-h")
+all.opts <- c("-x","-y","-cv","-JobID","-label","-h")
 for(i in 1:length(my.args)){
     if( i %% 2 == 1){
         if(!my.args[i] %in%  all.opts){
@@ -22,9 +21,9 @@ for(i in 1:length(my.args)){
         geno.name <- as.character(my.args[i+1])
     } else if(my.args[i] == "-y") {
         pheno.name <- as.character(my.args[i+1])
-    } else if(my.args[i] == "-c") {
+    } else if(my.args[i] == "-cv") {
         cvf.name <- as.character(my.args[i+1])
-    } else if(my.args[i] == "-JobId") {
+    } else if(my.args[i] == "-JobID") {
         cvf = as.numeric(my.args[i+1])
     } else if(my.args[i] == "-label") {
         my_ph <- as.character(my.args[i+1])      
@@ -32,7 +31,7 @@ for(i in 1:length(my.args)){
         print(" This script takes as a minimum two intputs\n")
         print(" -x genotypefile")
         print(" -y phenotypefile ")
-        print(" -v cross-valiadtion file : is optional if none is specified random 5 fold cv will be used")
+        print(" -cv cross-valiadtion file : is optional if none is specified random 5 fold cv will be used")
         print(" -JobID : specify column number to use in your cross validation file")
         print(" -label : use header of phenotype file column you want to use")
         quit()
--- gblub.r
+++ gblub.r
@@ -1,31 +1,33 @@
 ## gblub function used by ex.gblub
-## use: Rscript ex.gblup.r -x gent_geno.csv -y gent_pheno.csv -c gent_cvf.csv -JobId 2 -label value
-
-
+## use: Rscript ex.gblup.r -x gent_geno.csv -y gent_pheno.csv -c gent_cvf.csv obId 2 -label value
 
 gblup <- function(phenocsv, genocsv, cvfcsv, cvf=5, label = my_ph, inst = FALSE, req = TRUE, BLUP = TRUE, BL = FALSE, gen.plot = TRUE, loot=FALSE){
 
-    depends<- c('synbreed',"BGLR","doBy","doParallel","foreach","MASS","qtl","regress",'R.utils')
-    if(inst == TRUE)  
-        sapply(depends,function(X){install.packages(X)})
-    
-    if(req == TRUE)
-        foo <- sapply(depends,function(X){suppressPackageStartupMessages(library(X,character.only=TRUE))})
+    depends<- c('synbreed',"BGLR","doBy","doParallel","foreach","MASS","qtl","regress",'R.utils',"BBmisc","dplyr")
+    ## install packages that are not installed yet
+    foo <- sapply(depends,
+                  function(X){if(!suppressPackageStartupMessages(require(X,character.only = T))){install.packages(X)}})
 
-    rm(depends)
-    if(req == TRUE)
+    if(req == TRUE){
+        foo <- sapply(depends,function(X){suppressPackageStartupMessages(library(X,character.only=TRUE))})
         rm(foo)
+        }
+    rm(depends)
+
     
     geno.csv <- read.csv(genocsv)
     pheno.csv <- read.csv(phenocsv)
-    
-    ## init cross-validation
+       ## init cross-validation
     if(is.character(cvfcsv)){
        cvf.csv <- read.csv(cvfcsv)
        my_cvf <- cvf.csv[,cvf+1]
     }
-    if(is.na(cvfcsv))
+    if(is.na(cvfcsv)){
+        cvf = sample(1:999,1)
+        set.seed(cvf)
         my_cvf = sample(1:5,dim(pheno.csv)[1],replace = T)
+        cvfcsv <- "random 5-fold"
+    }
     if(loot){
         my_cvf = 1:(dim(pheno.csv)[1])
     }
@@ -34,7 +36,7 @@ gblup <- function(phenocsv, genocsv, cvfcsv, cvf=5, label = my_ph, inst = FALSE,
     if(any(colnames(pheno.csv)== label)){
         index <- which(colnames(pheno.csv) == label)
     }else{
-        cat("you used a wrong phenotype")
+        cat("you used a wrong phenotype\n")
         quit()
     }
     
@@ -46,14 +48,16 @@ gblup <- function(phenocsv, genocsv, cvfcsv, cvf=5, label = my_ph, inst = FALSE,
     my_geno <- as.data.frame(geno.csv[,2:dim(geno.csv)[2]], col.names = colnames(geno.csv),row.names=rownames(geno.csv))
     my_geno <- as.matrix(my_geno)
 
+
     for(i in 1:max(my_cvf)){
-        cat("Predicting cv fold", i, "\n")
+        cat("Predicting cv fold", i)
         my_gp <- create.gpData(pheno=my_pheno[my_cvf != i,],geno=my_geno,cores=2)
         myC <- codeGeno(my_gp)
         K <- kin(myC, ret="realized")/2
         mod1 <- gpMod(myC,model="BLUP",kin=K,markerEffects=TRUE,trait=2,predict=TRUE)
         intercept <- mod1$fit$beta[,1]
         my_pheno$pred[my_cvf == i] <- (mod1$prediction + intercept)
+        cat("   done\n")
     }
 
     if(gen.plot==TRUE){
@@ -67,7 +71,30 @@ gblup <- function(phenocsv, genocsv, cvfcsv, cvf=5, label = my_ph, inst = FALSE,
     print(c("MSE: ",mse))
     correlation = cor(my_pheno$pred, my_pheno$Yield)
     print(c("correlation: ",correlation))
+
+
+    
+    if(!any(dir() == "gb_results.csv")){
+        res <- as.data.frame(matrix(ncol=6, nrow = 1)) %>%
+            setColNames(c("geno","pheno","cv","label","ID", "cor"))
+        if(loot== F){
+            res[1,] <- c(genocsv,phenocsv,cvfcsv,label,cvf,correlation)
+        } else {
+            res[1,] <- c(genocsv,phenocsv,"loo",label,"loo",correlation)
+        } 
+        write.csv(res,"gb_results.csv")
+       
+    }else{
+        res <- read.csv("gb_results.csv",row.names = 1)
+        if(loot){
+            res[dim(res)[1]+1,] <- c(genocsv,phenocsv,"loo",as.character(label),"loo",correlation)
+        }else{
+            res[dim(res)[1]+1,] <- c(genocsv,phenocsv,cvfcsv,as.character(label),cvf,correlation)
+        }
+        write.csv(res,"gb_results.csv")
+    }
 }
+    
 
 
 
